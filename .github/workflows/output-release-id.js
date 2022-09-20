c1cc36dbff432c31ca53595e0389cf643b2e0f28
oplik0 <opliko.reg@protonmail.com>
2022-09-20 23:33:27 +0200

    fix: filter releases

--- .github/workflows/output-release-id.js
+++ .github/workflows/output-release-id.js
@@ -1,7 +1,3 @@
 import * as core from "@actions/core";
 
-const releases = JSON.parse(process.argv[2]);
-core.setOutput(
-	"release_id",
-	releases.reduce((acc, release) => release.id ? release.id : acc, false),
-);
+core.setOutput("release_id", process.argv[2]);
