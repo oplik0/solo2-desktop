7e3a83000fac44a71ba4375cf842bb17d16e5854
oplik0 <opliko.reg@protonmail.com>
2022-09-20 23:28:21 +0200

    fix: make release id work perhaps

--- .github/workflows/output-release-id.js
+++ .github/workflows/output-release-id.js
@@ -1,3 +1,7 @@
 import * as core from "@actions/core";
 
-core.setOutput("release_id", process.argv[2]);
+const releases = JSON.loads(process.argv[2]);
+core.setOutput(
+	"release_id",
+	releases.reduce((acc, release) => release.id ? release.id : acc, false),
+);
