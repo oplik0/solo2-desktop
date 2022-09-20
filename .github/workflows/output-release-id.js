ca581a288e3ae3349f189549dbeea842507414f5
oplik0 <opliko.reg@protonmail.com>
2022-09-20 23:29:26 +0200

    fix: it's not python

--- .github/workflows/output-release-id.js
+++ .github/workflows/output-release-id.js
@@ -1,6 +1,6 @@
 import * as core from "@actions/core";
 
-const releases = JSON.loads(process.argv[2]);
+const releases = JSON.parse(process.argv[2]);
 core.setOutput(
 	"release_id",
 	releases.reduce((acc, release) => release.id ? release.id : acc, false),
