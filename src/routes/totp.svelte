d7b1b07f85c61d1d43ae546159c55cbd752647e1
Opliko <opliko.reg@protonmail.com>
2023-12-26 16:52:55 +0100

    fix: make OATH work if there are no keys yet

--- src/routes/totp.svelte
+++ src/routes/totp.svelte
@@ -4,10 +4,16 @@
 	import { listen } from "@tauri-apps/api/event";
 	import { invoke } from "@tauri-apps/api/tauri";
 	import { onMount } from "svelte";
+	import type { Solo2List } from "$types";
 	export let params: Record<string, unknown> = {};
-	let oathList: Record<string, string>;
+	let oathList: Record<string, string> | undefined;
+	let keyList: Solo2List | undefined;
 	async function refreshOathList() {
 		oathList = await invoke("list_oath");
+		if (!oathList || Object.keys(oathList).length === 0) {
+			oathList = undefined;
+			keyList = await invoke("list_keys");
+		}
 	}
 	listen("usb_change", refreshOathList);
 	listen("oath_change", refreshOathList);
@@ -21,14 +27,14 @@
 
 <section>
 	{#if oathList}
-		{#each Object.entries(oathList).filter(([_, uuid]) => !params.uuid || uuid === params.uuid) as [credential, uuid]}
+		{#each Object.entries(oathList ?? {}).filter(([_, uuid]) => !params.uuid || uuid === params.uuid) as [credential, uuid]}
 			<TotpCard {credential} {uuid} />
 		{/each}
 	{/if}
 	<AddOath
-		uuid={Object.values(oathList ?? {}).filter(
-			(uuid) => !params.uuid || uuid === params.uuid
-		)}
+		uuid={Object.values(oathList ?? keyList ?? {})
+			.map((val) => (typeof val === "string" ? val : val.uuid))
+			.filter((uuid) => !params.uuid || uuid === params.uuid)}
 	/>
 </section>
 
