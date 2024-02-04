82937cdc5e5403fa42267a61713ee0603b5dddb5
Opliko <opliko.reg@protonmail.com>
2024-02-04 21:19:40 +0100

    feat: remove whitespace and pad OATH secrets before registering them
    fixes #111
    fixes #113

--- src/lib/AddOath.svelte
+++ src/lib/AddOath.svelte
@@ -32,7 +32,9 @@
 			uuid: selected_uuid,
 			label,
 			issuer: issuer.length > 0 ? issuer : undefined,
-			secret,
+			secret: secret
+				.replace(/\s/g, "")
+				.padEnd(8 * Math.ceil(secret.length / 8), "="),
 			kind,
 			algorithm,
 			period,
